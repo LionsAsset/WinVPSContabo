b7bed3b26139d330a553ebf4da3d4258fcda8a9b
Thiago <123501664+LionsAsset@users.noreply.github.com>
2024-11-27 08:13:12 -0300

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -119,7 +119,7 @@ if [[ "$download_choice" == "Y" || "$download_choice" == "y" ]]; then
     # Ask for the URL to download Windows.iso
     read -p "Enter the URL for Windows.iso (leave blank to use default): " windows_url
     if [ -z "$windows_url" ]; then
-        windows_url="https://bit.ly/3UGzNcB"  # Replace with the actual default URL
+        windows_url="https://archive.org/download/win-10_202411/Win10.iso"  # Replace with the actual default URL
     fi
     
     wget -O Windows.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" "$windows_url"
@@ -149,7 +149,7 @@ if [[ "$download_choice" == "Y" || "$download_choice" == "y" ]]; then
     # Ask for the URL to download Virtio.iso
     read -p "Enter the URL for Virtio.iso (leave blank to use default): " virtio_url
     if [ -z "$virtio_url" ]; then
-        virtio_url="https://bit.ly/4d1g7Ht"  # Replace with the actual default URL
+        virtio_url="https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.266-1/virtio-win-0.1.266.iso"  # Replace with the actual default URL
     fi
     
     wget -O Virtio.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" "$virtio_url"
